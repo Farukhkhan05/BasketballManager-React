e31d303a1d0f6a02111a88c8afb6690b204caf62
Farukhkhan05 <faf05.khan@gmail.com>
2021-04-22 18:42:52 +0530

    compose latest-push

--- src/components/ComposeTeam.jsx
+++ src/components/ComposeTeam.jsx
@@ -6,11 +6,11 @@ import TextField from '@material-ui/core/TextField';
 const ComposeTeam = () => {
 
     let options=[
-        {value:"Point Guard(PG)", label :"Point Guard"},
-        {value:"Shooting Guard(SG)",label :"Shooting Guard"},
-        {value:"Small Forward(SF)", label :"Small Forward"},
-        {value:"Power Forward(PF)",label :"Power Forward"},
-        {value:"The Center(C)", label:"The Center"}
+        {value:"Point Guard (PG)", label :"Point Guard (PG)"},
+        {value:"Shooting Guard (SG)",label :"Shooting Guard (SG)"},
+        {value:"Small Forward (SF)", label :"Small Forward (SF)"},
+        {value:"Power Forward (PF)",label :"Power Forward (PF)"},
+        {value:"The Center (C)", label:"The Center (C)"}
     ]
 
     const animatedComponents = makeAnimated();
@@ -28,15 +28,25 @@ const ComposeTeam = () => {
         console.log("values", values)
     }
 
+    const handleSelectChange = (e) =>{
+        console.log(e)
+        values.position = e
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log("form Submitted")
+    }
+
     return (
         <>
             <div className="compose_form">
-                <form>
+                <form onSubmit={handleSubmit} autoComplete="off">
                     <TextField value={values.fname} onChange={handleInputChange} name="fname" label ="First Name" variant="outlined" className="input"/>
                     <TextField value={values.lname} onChange={handleInputChange} name="lname" label ="Last Name" variant="outlined" className="input"/>
                     <TextField value={values.height} onChange={handleInputChange} name="height" label ="Height" variant="outlined" className="input" />
-                    <Select value={values.position} onChange={handleInputChange} name="position" components={animatedComponents} options={options} className="input"/>
-                    <button type = "submit" className="submitbtn">Save</button>
+                    <Select value={values.position} onChange={handleSelectChange} name="position" components={animatedComponents} options={options} isMulti placeholder="Select Position" className="input"/>
+                    <button type = "submit" value="submit" className="submitbtn">Save</button>
                 </form>
             </div>
         </>
