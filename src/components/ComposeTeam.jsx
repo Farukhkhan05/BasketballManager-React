0de176760302ff2d0f888a6feb24e1d583f4f085
Farukhkhan05 <faf05.khan@gmail.com>
2021-04-22 16:13:14 +0530

    Basketballmanager latest_push

--- src/components/ComposeTeam.jsx
+++ src/components/ComposeTeam.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+import TextField from '@material-ui/core/TextField';
 
 const ComposeTeam = () => {
+
+    let options=[
+        {value:"Point Guard(PG)", label :"Point Guard"},
+        {value:"Shooting Guard(SG)",label :"Shooting Guard"},
+        {value:"Small Forward(SF)", label :"Small Forward"},
+        {value:"Power Forward(PF)",label :"Power Forward"},
+        {value:"The Center(C)", label:"The Center"}
+    ]
+
+    const animatedComponents = makeAnimated();
+
+    const [values, setValues] = useState({
+        fname : "",
+        lname : "",
+        height : "",
+        position : ""
+    })
+
+    const handleInputChange = (e) =>{
+        const {name, value} = e.target;
+        setValues({...values, [name] : value})
+        console.log("values", values)
+    }
+
     return (
-        <div>
-            <h1>Compose Team</h1>
-        </div>
+        <>
+            <div className="compose_form">
+                <form>
+                    <TextField value={values.fname} onChange={handleInputChange} name="fname" label ="First Name" variant="outlined" className="input"/>
+                    <TextField value={values.lname} onChange={handleInputChange} name="lname" label ="Last Name" variant="outlined" className="input"/>
+                    <TextField value={values.height} onChange={handleInputChange} name="height" label ="Height" variant="outlined" className="input" />
+                    <Select value={values.position} onChange={handleInputChange} name="position" components={animatedComponents} options={options} className="input"/>
+                    <button type = "submit" className="submitbtn">Save</button>
+                </form>
+            </div>
+        </>
     )
 }
 
