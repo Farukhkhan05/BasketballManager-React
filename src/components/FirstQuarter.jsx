5f43250260951e3e97b21316af4521c219900c37
Farukhkhan05 <faf05.khan@gmail.com>
2021-04-23 18:37:08 +0530

    latest-commit

--- src/components/FirstQuarter.jsx
+++ src/components/FirstQuarter.jsx
@@ -2,9 +2,64 @@ import React from 'react'
 
 const FirstQuarter = () => {
     return (
-        <div>
-            <h1>First Quarter</h1>
-        </div>
+        <>
+            <div>
+                <form>
+                    <ol>
+                        <li>
+                            <div>
+                                <select name = "name1" placeholder = "Select Player" label = "Name">
+                                    <option>Select Player</option>
+                                </select>
+                                <select name = "position1" placeholder = "Select Position" label = "Position">
+                                    <option>Select Position</option>
+                                </select>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <select name = "name2" placeholder = "Select Player" label = "Name">
+                                    <option>Select Player</option>
+                                </select>
+                                <select name = "position2" placeholder = "Select Position" label = "Position">
+                                    <option>Select Position</option>
+                                </select>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <select name = "name3" placeholder = "Select Player" label = "Name">
+                                    <option>Select Player</option>
+                                </select>
+                                <select name = "position3" placeholder = "Select Position" label = "Position">
+                                    <option>Select Position</option>
+                                </select>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <select name = "name4" placeholder = "Select Player" label = "Name">
+                                    <option>Select Player</option>
+                                </select>
+                                <select name = "position4" placeholder = "Select Position" label = "Position">
+                                    <option>Select Position</option>
+                                </select>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <select name = "name5" placeholder = "Select Player" label = "Name">
+                                    <option>Select Player</option>
+                                </select>
+                                <select name = "position5" placeholder = "Select Position" label = "Position">
+                                    <option>Select Position</option>
+                                </select>
+                            </div>
+                        </li>
+                    </ol>
+                </form>
+            </div>
+        </>
     )
 }
 
