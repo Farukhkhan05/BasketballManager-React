0b84e260d01c86ffd5f44f009ec1a4a2c644f36f
Farukhkhan05 <faf05.khan@gmail.com>
2021-04-26 17:43:22 +0530

    latest-push

--- src/components/FirstQuarter.jsx
+++ src/components/FirstQuarter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FirstQuarter = () => {
     return (
         <>
-            <div>
+            <div className ="first_quarter">
                 <form>
                     <ol>
                         <li>
